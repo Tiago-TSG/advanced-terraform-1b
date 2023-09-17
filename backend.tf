adb7222fda5dba4686029850f641a07747357a26
Tiago Gonçalves <tiago.goncalves@sanar.com>
2023-09-17 18:43:34 -0300

    Adding remote state file

--- backend.tf
+++ backend.tf
@@ -4,8 +4,8 @@
 
 terraform {
   backend "s3" {
-    bucket = "github-terraform-state"
-    key    = "github-terraform-state/advanced-terraform-1b/production/terraform.tfstate"
+    bucket = "github-advanced-terraform-state"
+    key    = "github-advanced-terraform-state/advanced-terraform-1b/production/terraform.tfstate"
     region = "us-east-1"
   }
 }
